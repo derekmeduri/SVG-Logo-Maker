7c96981ffd656b4c4812df682e9d24f4e22ce65b
Derek Meduri <derekmeduri@gmail.com>
2023-07-16 21:08:20 -0400

    fixing test page

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const questions = [
   },
   {
     type: "input",
-    name: "text color",
+    name: "textColor",
     message:
       "Please enter a color or a hexidecimal number for your text color.",
   },
@@ -24,61 +24,56 @@ const questions = [
   },
   {
     type: "input",
-    name: "shape color",
+    name: "shapeColor",
     message:
       "Please enter a color or a hexidecimal number for your shape's fill/background color.",
   },
 ];
 
 //initialize app
-function init() {
-  inquirer
-    .prompt(questions)
-    .then((response) => {
-      const text = response.text;
-      const textColor = response.textcolor;
-      const shape = response.shape;
-      const shapeColor = response.shapeColor;
+inquirer
+  .prompt(questions)
+  .then((response) => {
+    const text = response.text;
+    const textColor = response.textcolor;
+    const shape = response.shape;
+    const shapeColor = response.shapeColor;
 
-      generateLogo(text, textColor, shape, shapeColor);
-    })
-    .catch((err) => console.log(err));
+    generateLogo(text, textColor, shape, shapeColor);
+  })
+  .catch((err) => console.log(err));
 
-  function generateLogo(text, textColor, shape, shapeColor) {
-    if (shape === "Triangle") {
-      const Triangle = new Triangle(text, textColor, shapeColor);
-      return fs.writeFile("logo.svg", Triangle.render(), (err) => {
-        if (err) {
-          console.info(err);
-        } else {
-          console.log("Generated logo.svg");
-        }
-      });
-    }
+function generateLogo(text, textColor, shape, shapeColor) {
+  if (shape === "Triangle") {
+    const Triangle = new Triangle(text, textColor, shapeColor);
+    return fs.writeFile("logo.svg", Triangle.render(), (err) => {
+      if (err) {
+        console.info(err);
+      } else {
+        console.log("Generated logo.svg");
+      }
+    });
+  }
 
-    if (shape === "Circle") {
-      const Circle = new Circle(text, textColor, shapeColor);
-      return fs.writeFile("logo.svg", Circle.render(), (err) => {
-        if (err) {
-          console.info(err);
-        } else {
-          console.log("Generated logo.svg");
-        }
-      });
-    }
+  if (shape === "Circle") {
+    const Circle = new Circle(text, textColor, shapeColor);
+    return fs.writeFile("logo.svg", Circle.render(), (err) => {
+      if (err) {
+        console.info(err);
+      } else {
+        console.log("Generated logo.svg");
+      }
+    });
+  }
 
-    if (shape === "Square") {
-      const Square = new Square(text, textColor, shapeColor);
-      return fs.writeFile("logo.svg", Square.render(), (err) => {
-        if (err) {
-          console.info(err);
-        } else {
-          console.log("Generated logo.svg");
-        }
-      });
-    }
+  if (shape === "Square") {
+    const Square = new Square(text, textColor, shapeColor);
+    return fs.writeFile("logo.svg", Square.render(), (err) => {
+      if (err) {
+        console.info(err);
+      } else {
+        console.log("Generated logo.svg");
+      }
+    });
   }
 }
-
-//calling app
-init();
