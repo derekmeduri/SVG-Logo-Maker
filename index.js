96dc4cbe8b289172ceeffd90d7be9c13c599abf3
Derek Meduri <derekmeduri@gmail.com>
2023-07-21 12:43:17 -0400

    debugging

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //required packages
 const CLI = require("./lib/cli");
-const Shape = require("./lib/shapes");
+const Shape = require("./lib/shapes.js");
 
 // creating new cli to run prompt from cli file
 const cli = new CLI();
